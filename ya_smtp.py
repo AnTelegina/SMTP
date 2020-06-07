f8a714b05a754fc2913251d232a8ef7b1e78f9fc
Anastasia <anastasia_telegina@mail.ru>
2020-06-07 09:20:56 +0500

    parse config file

--- ya_smtp.py
+++ ya_smtp.py
@@ -12,3 +12,28 @@ target = USER_NAME
 user_name = USER_NAME
 password =  PASSWORD
 
+def parse_file_configs():
+    user = ''
+    targets = None
+    passwd = ''
+    subject = ''
+    attachments = None
+
+    with open('configs.txt', encoding='utf-8') as config_file:
+        for line in config_file:
+            if line.find("from:") == 0:
+                user = line.split(' ')[1].strip()
+            if line.find("to:") == 0:
+                targets = line[4:].strip().split(' ')
+            if line.find("password:") == 0:
+                passwd = line.split(' ')[1].strip()
+            if line.find("subject:") == 0:
+                subject = line.split(' ')[1].strip()
+            if line.find("attachments:") == 0:
+                attachments = line[13:].strip().split(' ')
+        if user == '' or targets is None or passwd == '':
+            sys.exit(-1)
+
+        return user, passwd, subject, targets, attachments
+
+parse_file_configs()
